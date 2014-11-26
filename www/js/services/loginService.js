bb283e36098055a0ceb5b6b4bab1d052ac150c85
jaxox <wl.jason.yu@gmail.com>
2014-11-25 23:42:28 -0800

    after login, shows success / error message in console.

--- www/js/services/loginService.js
+++ www/js/services/loginService.js
@@ -3,58 +3,26 @@
  */
  'use strict';
 
-//app.factory('loginService', function($http){
-//    return{
-//        login:function(user){
-//            console.log("enter login service");
-//            var $promise=$http.post('http://localhost:8080/api/login',JSON.stringify(user)); //send data to the api
-//
-//            //testing only
-//            $promise.then(function(msg){
-//                if(msg.data=='succes') console.log('succes login');
-//                else console.log('error login');
-//            });
-//
-//        }
-//    }
-//});
-
-//app.factory('loginService', function($http){
-//
-//    var factory ={};
-//
-//    factory.login = function(user){
-//        console.log("enter login service");
-//        var $promise=$http.post('http://localhost:8080/api/login',JSON.stringify(user)); //send data to the api
-//
-//        //testing only
-//        $promise.then(function(msg){
-//            if(msg.data=='succes') console.log('succes login');
-//            else console.log('error login');
-//        });
-//
-//    }
-//
-//    return factory;
-//});
-
-app.factory('loginService', function($http, $ionicModal){
+app.factory('loginService', function($http){
 
     var factory ={};
 
-    factory.doLogin = function(user){
+    factory.login = function(userReqObj){
         console.log("enter login service");
-        var $promise=$http.post('http://localhost:8080/api/login',JSON.stringify(user)); //send data to the api
+        var $promise=$http.post('http://localhost:8080/api/login',JSON.stringify(userReqObj)); //send data to the api
 
-        //testing only
-        $promise.then(function(msg){
-            if(msg.data=='succes') console.log('succes login');
-            else console.log('error login');
-        });
-
+        $promise.
+            success(function(data, status, headers, config) {
+                // this callback will be called asynchronously
+                // when the response is available
+                console.log("success login, ID:" + data.id );
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                console.log("error login, login:" + userReqObj.login);
+            });
     }
-
-
 
     return factory;
 });
